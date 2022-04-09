c855a20acda799b3651a072fc4dbbd8ba926e5ce
Jordi <damlabulbul26@gmail.com>
2022-04-09 02:09:56 -0700

    started on models

---
+++ models/ProductTag.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
+    },
+ 
---
+++ models/index.js
@@ -0,0 +1,20 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  }
+});
+
